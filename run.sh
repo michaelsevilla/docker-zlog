5f8b33068321b6e434f618e3ca52dcc62980c00a
Michael Sevilla <mikesevilla3@gmail.com>
2015-10-23 23:49:17 -0700

    Compiles but need to figure out how to copy in rados-classes
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# this as tutumcloud/tutum-ubuntu plus our mods for taking the sshd
-# port number from an env variable
-
-echo "HI"
--- standalone.sh
+++
@@ -1,3 +0,0 @@
-docker ps -aq | xargs docker stop 
-docker ps -aq | xargs docker rm
-docker run --name zlog -it zlog-test /bin/bash
